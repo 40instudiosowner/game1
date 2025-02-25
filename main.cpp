b833c6edefc210b5546ad8af43ba5c3cd48c5dd0
unknown <anton.sorokin030529@gmail.com>
2025-02-25 20:47:44 +0300

    simple window

--- main.cpp
+++ main.cpp
@@ -1,2 +1,76 @@
-#include <GLFW\glfw3.h>
-#include <GL\glew.h>
+// GLEW нужно подключать до GLFW.
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+	// и приложение после этого закроется
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int main()
+{
+	//Инициализация GLFW
+	glfwInit();
+	//Настройка GLFW
+	//Задается минимальная требуемая версия OpenGL. 
+	//Мажорная 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//Минорная
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Установка профайла для которого создается контекст
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Выключение возможности изменения размера окна
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Game 1", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height); // get width & height from window by reference
+
+	glViewport(0, 0, width, height); // отрисовать 
+
+	while (!glfwWindowShouldClose(window)) // игровой цикл
+	{
+
+		glfwPollEvents();  // Проверяем события и вызываем функции обратного вызова
+
+		// Команды отрисовки
+		glClearColor(0.f, 0.9f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);  // Меняем буферы местами
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
