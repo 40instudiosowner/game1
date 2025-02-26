8bba0fa192f0801e5407ff4e2c6c0c41be785d08
unknown <anton.sorokin030529@gmail.com>
2025-02-26 13:30:58 +0300

    rectangle

--- main.cpp
+++ main.cpp
@@ -105,13 +105,19 @@ int main()
 
 	GLfloat vertices[] =
 	{
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,  // Верхний правый угол
+		0.5f, -0.5f, 0.0f,  // Нижний правый угол
+		-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+		-0.5f,  0.5f, 0.0f   // Верхний левый угол
 	};
-
-	GLuint VBO, VAO;
+	GLuint indices[] = 
+	{  // Помните, что мы начинаем с 0!
+		0, 1, 3,   // Первый треугольник
+		1, 2, 3    // Второй треугольник
+	};
+	GLuint VBO, VAO, EBO;
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// ..:: Код инициализации (выполняется единожды (если, конечно, объект не будет часто изменяться)) :: .. 
 	// 1. Привязываем VAO;
 	glGenVertexArrays(1, &VAO);
@@ -120,15 +126,19 @@ int main()
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 3. Устанавливаем указатели на вершинные атрибуты 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	//glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 	//4. Отвязываем VAO
 	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 2 картинки
 
 	while (!glfwWindowShouldClose(window)) // игровой цикл
 	{
@@ -139,10 +149,10 @@ int main()
 		glClearColor(0.f, 0.9f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// triangle
+		// rectangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 
