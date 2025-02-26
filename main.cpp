64ede520f768dc401f3903191764b35b74bba0fa
unknown <anton.sorokin030529@gmail.com>
2025-02-26 13:07:31 +0300

    first triangle

--- main.cpp
+++ main.cpp
@@ -15,6 +15,13 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+const GLchar* vertexShaderSource{
+	"#version 330 core\nlayout(location = 0) in vec3 position;\nvoid main()\n{\ngl_Position = vec4(position.x, position.y, position.z, 1.0);\n}\n\0"
+}; // source code vertex shader
+const GLchar* fragmentShaderSource{
+	"#version 330 core\nout vec4 color;\nvoid main()\n{\ncolor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}\n\0"
+}; // source code fragment shader
+
 
 int main()
 {
@@ -41,21 +48,87 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-
 	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	//glewInit();
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height); // get width & height from window by reference
+	glViewport(0, 0, width, height); // отрисовать 
+
 
 	glfwSetKeyCallback(window, key_callback);
 
 
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height); // get width & height from window by reference
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
-	glViewport(0, 0, width, height); // отрисовать 
+	GLint success; // check the build
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader;  // fragment shader
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::LINK::SHADERS::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);   // после связывания больше не нужны
+	glDeleteShader(fragmentShader);
+
+	GLfloat vertices[] =
+	{
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	// ..:: Код инициализации (выполняется единожды (если, конечно, объект не будет часто изменяться)) :: .. 
+	// 1. Привязываем VAO;
+	glGenVertexArrays(1, &VAO);
+	// 2. Копируем наш массив вершин в буфер для OpenGL
+		// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. Устанавливаем указатели на вершинные атрибуты 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	//4. Отвязываем VAO
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window)) // игровой цикл
 	{
@@ -66,10 +139,18 @@ int main()
 		glClearColor(0.f, 0.9f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 
 		glfwSwapBuffers(window);  // Меняем буферы местами
 	}
-
+	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 
 	return 0;
